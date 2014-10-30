0816443e812b4d029b5b03575fa4663999096116
David Perry <curiousepic@gmail.com>
2014-10-29 22:59:09 -0400

    Some styling

--- app/assets/javascripts/hub.js
+++ app/assets/javascripts/hub.js
@@ -5,13 +5,13 @@ $(document).on('ready', function () {
         $('#gethub-profile-title')
             .replaceWith('<h2><a href="' + data.html_url + '">' + data.login + '</a></h2>');
         $('#gethub-profile')
-            .append('<img src="' + data.avatar_url + '" height="300" width="300">')
+            .append('<img src="' + data.avatar_url + '" height="200" width="200">')
             .append('<p>True Name: ' + data.name + '</p>')
-            .append('<p>Good Company: ' + data.company + '</p>')
-            .append('<p>Resides in: ' + data.location + '</p>')
-            .append('<p>Reach me: ' + data.email + '</p>')
-            .append('<p>Read me: <a href="' + data.blog + '">twitter.com/curiousepic</a></p>')
-            .append('<p>Joined GitHub: ' + data.created_at + '</p>')
+            .append('<p>Manufacturer: ' + data.company + '</p>')
+            .append('<p>Demesne: ' + data.location + '</p>')
+            .append('<p>Reach: <a href="mailto:' + data.email + '">' + data.email + '</a></p>')
+            .append('<p>Read: <a href="' + data.blog + '">twitter.com/curiousepic</a></p>')
+            .append('<p>Joined: ' + data.created_at + '</p>')
             .append('<p>Repos: ' + data.public_repos + '</p>')
             .append('<p>Followers: ' + data.followers + '</p>')
             .append('<p>Following: ' + data.following + '</p>');
@@ -32,7 +32,8 @@ $(document).on('ready', function () {
                     .append('<p>Main Language: ' + repo.language + '</p>')
                     .append('<p>Stargazers: ' + repo.stargazers_count + '</p>')
                     .append('<p>Forks: ' + repo.forks_count + '</p>')
-                    .append('<p>Last Updated: ' + repo.updated_at + '</p>');
+                    .append('<p>Last Updated: ' + repo.updated_at + '</p>')
+                    .append('<br>');
 
 //                function fork(f) {
 //                    if (f) {
